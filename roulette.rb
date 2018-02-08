f1ad19d88e9057736cc4ec3cf663a091689e57c9
Parker <parkermatheson@gmail.com>
2018-02-08 00:51:18 -0700

    Cleaned up the code to comply with Ruby Style Guide, and commented out puts statements

--- roulette.rb
+++ roulette.rb
@@ -1,7 +1,10 @@
 # PLAY ROULETTE
 
 # setup wheel
-@wheel = [0, 28, 9, 26, 30, 11, 7, 20, 32, 17, 5, 22, 34, 15, 3, 24, 36, 13, 1, '00', 27, 10, 25, 29, 12, 8, 19, 31, 18, 6, 21, 33, 16, 4, 23, 35, 14, 2]
+@wheel = [
+  0, 28, 9, 26, 30, 11, 7, 20, 32, 17, 5, 22, 34, 15, 3, 24, 36, 13, 1,
+  '00', 27, 10, 25, 29, 12, 8, 19, 31, 18, 6, 21, 33, 16, 4, 23, 35, 14, 2
+]
 
 # setup payout rules for bets in a hash
 
@@ -17,62 +20,63 @@
 # etc...
 
 @payout = {
-  "even" => 1,
-  "odd" => 1,
-  "red" => 1,
-  "black" => 1,
-  "0" => 35,
-  "00" => 35,
-  "1" => 35,
-  "2" => 35,
-  "3" => 35,
-  "4" => 35,
-  "5" => 35,
-  "6" => 35,
-  "7" => 35,
-  "8" => 35,
-  "9" => 35,
-  "10" => 35,
-  "11" => 35,
-  "12" => 35,
-  "13" => 35,
-  "14" => 35,
-  "15" => 35,
-  "16" => 35,
-  "17" => 35,
-  "18" => 35,
-  "19" => 35,
-  "20" => 35,
-  "21" => 35,
-  "22" => 35,
-  "23" => 35,
-  "24" => 35,
-  "25" => 35,
-  "26" => 35,
-  "27" => 35,
-  "28" => 35,
-  "29" => 35,
-  "30" => 35,
-  "31" => 35,
-  "32" => 35,
-  "33" => 35,
-  "34" => 35,
-  "35" => 35,
-  "36" => 35,
-  "row" => 17,
-  "split" => 17,
-  "street" => 11,
-  "corner" => 8,
-  "first_column" => 2,
-  "second_column" => 2,
-  "third_column" => 2,
-  "first_dozen" => 2,
-  "second_dozen" => 2,
-  "third_dozen" => 2,
-  "1 to 18" => 1,
-  "19 to 36" => 1
+  'even' => 1,
+  'odd' => 1,
+  'red' => 1,
+  'black' => 1,
+  '0' => 35,
+  '00' => 35,
+  '1' => 35,
+  '2' => 35,
+  '3' => 35,
+  '4' => 35,
+  '5' => 35,
+  '6' => 35,
+  '7' => 35,
+  '8' => 35,
+  '9' => 35,
+  '10' => 35,
+  '11' => 35,
+  '12' => 35,
+  '13' => 35,
+  '14' => 35,
+  '15' => 35,
+  '16' => 35,
+  '17' => 35,
+  '18' => 35,
+  '19' => 35,
+  '20' => 35,
+  '21' => 35,
+  '22' => 35,
+  '23' => 35,
+  '24' => 35,
+  '25' => 35,
+  '26' => 35,
+  '27' => 35,
+  '28' => 35,
+  '29' => 35,
+  '30' => 35,
+  '31' => 35,
+  '32' => 35,
+  '33' => 35,
+  '34' => 35,
+  '35' => 35,
+  '36' => 35,
+  'row' => 17,
+  'split' => 17,
+  'street' => 11,
+  'corner' => 8,
+  'first_column' => 2,
+  'second_column' => 2,
+  'third_column' => 2,
+  'first_dozen' => 2,
+  'second_dozen' => 2,
+  'third_dozen' => 2,
+  '1 to 18' => 1,
+  '19 to 36' => 1
 }
-# if spin is an integer within the same category the user bet on, calculate the payout.
+# if spin is an integer within the same category the user bet on,
+# calculate the payout.
 # categories
 #   first_dozen
 #     (1..12)
@@ -110,8 +114,14 @@
   third_dozen: { payout: 2, nums: (25..36).to_a },
   odd: { payout: 1, nums: (1..35).step(2).to_a },
   even: { payout: 1, nums: (2..36).step(2).to_a },
-  red: { payout: 1, nums: [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36] },
-  black: { payout: 1, nums: [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35] },
+  red:  {
+    payout: 1, nums:
+    [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+  },
+  black: {
+    payout: 1, nums:
+    [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+  },
   one_to_eighteen: { payout: 1, nums: (1..18).to_a },
   nineteen_to_thirtysix: { payout: 1, nums: (1..18).to_a }
 }
@@ -138,15 +148,16 @@ def spin
   @ball = @wheel.sample
   p @ball
 end
-  # MATHS
-  # if spin is an integer within the same category the user bet on, calculate the payout.
-  # compare spin to all the integers within categories.player_bet_category
+# MATHS
+# if spin is an integer within the same category the user bet on,
+# calculate the payout.
+# compare spin to all the integers within categories.player_bet_category
 
 # if categories.player_bet_category.value?(spin)
-def maths(bet_name, bet_value)
+def maths
   # if @table[:@player_bet_category]
-  p @table.dig(@player_bet_category.to_sym, :nums)
-  p @ball
+  # p @table.dig(@player_bet_category.to_sym, :nums)
+  # p @ball
   if @table.dig(@player_bet_category.to_sym, :nums).include?(@ball)
     # if @ball == @player_bet_category
     winnings = @bet_value * @payout[@player_bet_category.to_s]
@@ -162,7 +173,7 @@ def turn
   while @wallet > 0
     bet
     spin
-    maths(@player_bet_category, @bet_value)
+    maths
     break if @wallet <= 0
   end
 end
